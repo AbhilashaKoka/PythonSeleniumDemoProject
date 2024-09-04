4c52e8a29d24250e3b2e58c266a94a9d1055c47a
akoka <akoka@altimetrik.com>
2024-09-04 11:17:13 +0530

    commit15

--- examples/features/steps/loginsteps.py
+++ examples/features/steps/loginsteps.py
@@ -1,7 +1,9 @@
+import logging
 import time
 from behave import given, when,then
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import logging as logger
 
 driver=webdriver.Chrome()
 
@@ -14,7 +16,7 @@ def step_impl(context):
     driver.execute_script('window.scrollBy(0, 300)')
     driver.find_element(By.XPATH,"//*[@class=\"category-cards\"]//following::div[@class=\"card-body\"]//h5[contains(text(),\"Elements\")]").click()
     driver.find_element(By.XPATH,"//*[@class=\"accordion\"]//div[@class=\"element-group\"]//following::span[@class=\"text\" and contains(text(),\"Text Box\")]").click()
-
+    logging.info("User is on Element TextBox Landing Page")
 
 
 @when(u'User enter details username, email, current address, permanent address')
@@ -24,22 +26,23 @@ def step_impl(context):
     driver.find_element(By.XPATH,"//*[@id='userEmail']").send_keys("sita@gmail.com")
     driver.find_element(By.XPATH,"//*[@id='currentAddress']").send_keys("adasffhlfhg")
     driver.find_element(By.XPATH,"//*[@id='permanentAddress']").send_keys("adsfsfafshf")
-
+    logging.info("User Enter Valid Details in InputFields")
 
 
 
 @when(u'Click on Submit')
 def step_impl(context):
     driver.find_element(By.XPATH,"//*[@id='submit']").submit()
+    logging.info("User Submit Details")
 
 
 @then(u'user should able to verify the detail on output area')
 def step_impl(context):
-    time.sleep(10)
-    elements=driver.find_elements(By.XPATH,"//*[@id=\"output\"]//p")
-    for menu in elements:
-        print(menu.text)
-        print("user is on verify page")
+    # time.sleep(10)
+    # elements=driver.find_elements(By.XPATH,"//*[@id=\"output\"]//p")
+    # for menu in elements:
+    # print(menu.text)
+    logging.info("User Verify Details Successfully")
 
 
 
