a78c38ceb248c8a406d17206936d51fe7276854c
akoka <akoka@altimetrik.com>
2024-09-06 16:38:25 +0530

    commit-24

--- examples/DemoPythonScript/StringFunction.py
+++ examples/DemoPythonScript/StringFunction.py
@@ -1,10 +1,19 @@
+# Function to reverse a number
 def reverse_number(n):
-    reverse_num = 0
+    # Initialize the reversed number to 0
+    reversed_num = 0
+    # Loop until the number is greater than 0
     while n > 0:
+        # Get the last digit of the number
         digit = n % 10
-        revered_num = reverse_num * 10 + digit
+        # Add the digit to the reversed number's correct place
+        reversed_num = reversed_num * 10 + digit
+        # Remove the last digit from the number
         n = n // 10
-        return revered_num
+    # Return the reversed number
+    return reversed_num
 
+# Number to be reversed
 number = 12345
+# Call the function and print the result
 print(reverse_number(number))
